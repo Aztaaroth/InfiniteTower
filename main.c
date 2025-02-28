fe69b58fd70074fd46a6b3c55e79e5ea7c0ee0d5
Aztaaroth <jimmy.lundstein@gmail.com>
2025-02-28 21:01:53 +0100

    Main game loop "done"

--- main.c
+++ main.c
@@ -2,15 +2,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 800
 #define GRID_SIZE 5
 #define ROOM_SIZE (SCREEN_WIDTH / GRID_SIZE)
-#define MAX_ROOMS 11
-#define MIN_ROOMS 7
+#define MAX_ROOMS 10
+#define MIN_ROOMS 6
+#define PLAYER_SPEED 2.0f
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 typedef struct Room {
     Vector2 position;
@@ -24,11 +27,10 @@ int totalRooms;
 Vector2 playerPos;
 Vector2 stairsPos;
 bool gamePaused = false;
-bool gameRestart = false;
 bool stairsPlaced = false;
 
-bool CheckRoomCollision(Vector2 a, Vector2 b) {
-    return (a.x == b.x && a.y == b.y);
+float Distance(Vector2 a, Vector2 b) {
+    return sqrtf((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 }
 
 void ConnectRooms(int indexA, int indexB) {
@@ -36,10 +38,10 @@ void ConnectRooms(int indexA, int indexB) {
     Vector2 b = (Vector2){rooms[indexB].position.x * ROOM_SIZE + ROOM_SIZE / 2, rooms[indexB].position.y * ROOM_SIZE + ROOM_SIZE / 2};
 
     if (a.x != b.x) {
-        DrawRectangle(MIN(a.x, b.x), a.y - 5, abs(b.x - a.x), 10, WHITE);
+        DrawRectangle(MIN(a.x, b.x) - 20, a.y - 20, abs(b.x - a.x) + 40, 40, LIGHTGRAY);
     }
     if (a.y != b.y) {
-        DrawRectangle(b.x - 5, MIN(a.y, b.y), 10, abs(b.y - a.y), WHITE);
+        DrawRectangle(a.x - 20, MIN(a.y, b.y) - 20, 40, abs(b.y - a.y) + 40, LIGHTGRAY);
     }
 }
 
@@ -52,31 +54,13 @@ void GenerateDungeon() {
         rooms[i].position = (Vector2){GetRandomValue(0, GRID_SIZE - 1), GetRandomValue(0, GRID_SIZE - 1)};
         rooms[i].visited = false;
         rooms[i].corridorConnections = 0;
-
         for (int j = 0; j < MAX_ROOMS; j++) {
             rooms[i].connectedTo[j] = -1;
         }
-
-        for (int j = 0; j < i; j++) {
-            if (CheckRoomCollision(rooms[i].position, rooms[j].position)) {
-                i--;  // Retry if collision happens
-                break;
-            }
-        }
     }
 
-    rooms[0].visited = true; // Start room
+    rooms[0].visited = true;
     playerPos = (Vector2){rooms[0].position.x * ROOM_SIZE + ROOM_SIZE / 2, rooms[0].position.y * ROOM_SIZE + ROOM_SIZE / 2};
-
-    for (int i = 1; i < totalRooms; i++) {
-        int connectedRoom = GetRandomValue(0, i - 1);
-        if (rooms[connectedRoom].corridorConnections < 3 && rooms[i].corridorConnections < 3) {
-            rooms[connectedRoom].connectedTo[rooms[connectedRoom].corridorConnections] = i;
-            rooms[i].connectedTo[rooms[i].corridorConnections] = connectedRoom;
-            rooms[connectedRoom].corridorConnections++;
-            rooms[i].corridorConnections++;
-        }
-    }
 
     int distanceRooms = GetRandomValue(4, totalRooms - 1);
     stairsPos = (Vector2){rooms[distanceRooms].position.x * ROOM_SIZE + ROOM_SIZE / 2, rooms[distanceRooms].position.y * ROOM_SIZE + ROOM_SIZE / 2};
@@ -87,15 +71,23 @@ void DrawDungeon() {
     for (int i = 0; i < totalRooms; i++) {
         Rectangle room = {rooms[i].position.x * ROOM_SIZE, rooms[i].position.y * ROOM_SIZE, ROOM_SIZE, ROOM_SIZE};
         DrawRectangleRec(room, LIGHTGRAY);
-        for (int j = 0; j < rooms[i].corridorConnections; j++) {
-            if (rooms[i].connectedTo[j] != -1) {
-                ConnectRooms(i, rooms[i].connectedTo[j]);
-            }
-        }
     }
-
+    for (int i = 0; i < totalRooms - 1; i++) {
+        ConnectRooms(i, i + 1);
+    }
     DrawCircleV(playerPos, 10, BLUE);
     if (stairsPlaced) DrawCircleV(stairsPos, 10, RED);
+}
+
+void UpdatePlayer() {
+    if (IsKeyDown(KEY_W)) playerPos.y -= PLAYER_SPEED;
+    if (IsKeyDown(KEY_S)) playerPos.y += PLAYER_SPEED;
+    if (IsKeyDown(KEY_A)) playerPos.x -= PLAYER_SPEED;
+    if (IsKeyDown(KEY_D)) playerPos.x += PLAYER_SPEED;
+
+    if (CheckCollisionPointCircle(playerPos, stairsPos, 10)) {
+        GenerateDungeon();
+    }
 }
 
 void PauseMenu() {
@@ -111,37 +103,21 @@ int main(void) {
     GenerateDungeon();
 
     while (!WindowShouldClose()) {
-        if (IsKeyPressed(KEY_P)) gamePaused = !gamePaused;
-        if (gamePaused) {
-            if (IsKeyPressed(KEY_R)) {
-                gameRestart = true;
-                gamePaused = false;
-            }
-            if (IsKeyPressed(KEY_ESCAPE)) break;
-        }
-
-        if (gameRestart) {
+        if (IsKeyPressed(KEY_SPACE)) gamePaused = !gamePaused;
+        if (gamePaused && IsKeyPressed(KEY_R)) {
             GenerateDungeon();
-            gameRestart = false;
+            gamePaused = false;
         }
-
         if (!gamePaused) {
-            if (IsKeyDown(KEY_RIGHT)) playerPos.x += 2;
-            if (IsKeyDown(KEY_LEFT)) playerPos.x -= 2;
-            if (IsKeyDown(KEY_DOWN)) playerPos.y += 2;
-            if (IsKeyDown(KEY_UP)) playerPos.y -= 2;
-
-            if (CheckCollisionPointCircle(playerPos, stairsPos, 10)) {
-                GenerateDungeon();
-            }
+            UpdatePlayer();
         }
 
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawDungeon();
-
         if (gamePaused) {
             PauseMenu();
+        } else {
+            DrawDungeon();
         }
         EndDrawing();
     }
